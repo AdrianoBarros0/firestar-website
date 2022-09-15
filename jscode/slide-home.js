1788f76843f26ea5449125e9446fd06a671da75d
Adriano Barroso <112637576+AdrianoBarros0@users.noreply.github.com>
2022-09-15 12:01:38 -0300

    Update slide-home.js

--- jscode/slide-home.js
+++ jscode/slide-home.js
@@ -1,7 +1,14 @@
 class SlideHome {
     constructor(id) {
         this.slide = document.querySelector(`[data-slide="${id}"]`);
+        this.active  = 0;
         console.log(this.slide);
+        this.activeSlide(2);
+    }
+    activeSlide(index){
+        this.active = index;
+        this.items = this.slide.querySelectorAll('.slide-items > *');
+        console.log(this.items);
     }
 }
 
